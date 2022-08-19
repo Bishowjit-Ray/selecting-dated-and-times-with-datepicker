6b7c1ccc569ebc63f9827a0be9e7cddad3fe698e
Bishowjit-Ray <82141518+Bishowjit-Ray@users.noreply.github.com>
2022-08-19 21:42:29 +0600

    Add files via upload

---
+++ entering numbers with slepper/entering numbers with slepper/entering_numbers_with_slepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  entering_numbers_with_slepperApp.swift
+//  entering numbers with slepper
+//
+//  Created by Bishowjit Ray on 19/8/22.
+//
+
+import SwiftUI
+
+@main
+struct entering_numbers_with_slepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
